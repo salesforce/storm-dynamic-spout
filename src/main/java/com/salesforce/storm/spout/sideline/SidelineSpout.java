5ed11bbdaa05943211fe1592de79092dd06f5abf
stephen powis <crim@users.noreply.github.com>
2017-03-09 13:08:06 -0500

    Remove import

--- src/main/java/com/salesforce/storm/spout/sideline/SidelineSpout.java
+++ src/main/java/com/salesforce/storm/spout/sideline/SidelineSpout.java
@@ -24,7 +24,6 @@ import org.apache.storm.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Clock;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
