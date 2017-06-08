7f98299f946f7034326f895c0fbfbd9e76c7ae0d
Stan Lemon <stanlemon@users.noreply.github.com>
2017-06-08 14:29:25 -0400

    Remove

--- src/main/java/com/salesforce/storm/spout/sideline/VirtualSpoutIdentifier.java
+++ src/main/java/com/salesforce/storm/spout/sideline/VirtualSpoutIdentifier.java
@@ -5,16 +5,4 @@ package com.salesforce.storm.spout.sideline;
  */
 public interface VirtualSpoutIdentifier {
 
-    /**
-     * Get the string form of the identifier.
-     * @return A string of the identifier
-     */
-    String toString();
-
-    /**
-     * Is this identifier equal to another.
-     * @param obj The other identifier
-     * @return Whether or not the two identifiers are equal
-     */
-    boolean equals(Object obj);
 }
